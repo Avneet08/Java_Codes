d95b6592fe22fd07fc78b36f20e26c7200d8e525
Avneet <avneet16233@iiitd.ac.in>
2018-05-09 03:18:08 +0530

    my first commiy

--- myfirstproject/src/lesson1/Rectangle.java
+++
@@ -1,7 +0,0 @@
-package lesson1;
-
-public class Rectangle extends Areapolygon {
-	public double Area (){
-		return (height*width);
-	}
-}
--- myfirstproject/src/lesson1/Trngl.java
+++
@@ -1,7 +0,0 @@
-package lesson1;
-
-public class Trngl extends Areapolygon{
-	public double Area (){
-		return (height*width)/2;
-	}
-}
